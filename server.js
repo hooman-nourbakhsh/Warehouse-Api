ba0d6ee85b64aaa65bfddf9344a34e1d49f9fca5
HoTaN <id4acc2014@protonmail.com>
2024-11-17 02:35:25 +0330

    build: :rocket: setup backend deployment on Vercel with server.js and vercel.json configuration

--- server.js
+++ server.js
@@ -27,20 +27,22 @@ app.use("/auth", authRoutes);
 
 // Helper function to start server and try the next port if the current one is taken
 function startServer(port) {
+  const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
+
   const server = app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${SERVER_URL}`);
 
     // Update Swagger with the correct port
     swaggerDocument.servers = [
       {
-        url: `http://localhost:${port}`,
-        description: "Local server",
+        url: SERVER_URL,
+        description: "Production server",
       },
     ];
 
     // Serve updated Swagger docs
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    console.log(`Swagger API docs are available at http://localhost:${port}/api-docs`);
+    console.log(`Swagger API docs are available at ${SERVER_URL}/api-docs`);
   });
 
   // Handle error in case the port is in use
